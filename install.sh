34d688a728d57a4a1e97c0c8a545c2dc07783220
sudoken <44742486+adieumonks@users.noreply.github.com>
2025-03-30 18:30:02 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 set -eu
 
 DOT_DIRECTORY=${PWD}
+declare -a DIRECTORIES=('.config .zsh')
 
 for f in .??*
 do
@@ -11,4 +12,16 @@ do
     [[ "$f" == ".DS_Store" ]] && continue
 
     ln -snfv "${DOT_DIRECTORY}/$f" "$HOME/$f"
+done
+
+for child_directory in $DIRECTORIES; do
+  cd "${DOT_DIRECTORY}/${child_directory}"
+
+  for directory in $(find . -type d); do
+    mkdir -p "${HOME}/${child_directory}/${directory}"
+  done
+
+  for file in $(find . -type f | grep -v "\.git"); do
+      ln -snfv "${DOT_DIRECTORY}/${child_directory}/${file:2}" "${HOME}/${child_directory}/${file:2}"
+  done
 done
