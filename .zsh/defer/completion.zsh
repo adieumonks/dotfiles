61ae35898dfd094404a4d4a5001120d718b11ac7
Ken Sudo <44742486+adieumonks@users.noreply.github.com>
2026-07-16 18:37:30 +0900

    feat: add gcloud completion

--- .zsh/defer/completion.zsh
+++ .zsh/defer/completion.zsh
@@ -6,5 +6,9 @@ else
   compinit -C;
 fi;
 
+# gcloudコマンド補完
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
 # kubectlコマンド補完
 source <(kubectl completion zsh)
